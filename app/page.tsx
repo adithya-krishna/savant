40ad973cd1f5d12c7da94b10e12643496c283273
swethaudupa <swethaudupag@gmail.com>
2025-04-27 17:35:02 +0530

    Fixed lint issues.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col">
